6ebb4f0b1a3ff0cc94234f8cba1dddd42390e83c
Lenovo <Lenovo@DESKTOP-U4GLDR7>
2022-01-26 20:55:06 +0530

    user1 1st commit

---
+++ src/test/java/com/crm/sdet/demoGit/DemoGit.java
@@ -0,0 +1,11 @@
+package com.crm.sdet.demoGit;
+
+import org.testng.annotations.Test;
+
+public class DemoGit {
+@Test
+public void demoGit()
+{
+	System.out.println("Hi this is user 1 first commit");
+}
+}
